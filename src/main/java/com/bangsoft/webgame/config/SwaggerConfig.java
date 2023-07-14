e370ed651ba558e3d89c90f7d58d0cd1ea494c03
byeongjuPark <bottlepark28@gmail.com>
2023-07-14 17:34:57 +0900

    초기 환경 구성

---
+++ src/main/java/com/bangsoft/webgame/config/SwaggerConfig.java
@@ -0,0 +1,84 @@
+package com.bangsoft.webgame.config;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+@Configuration
+public class SwaggerConfig {
+
+    // Docket 문서화 객체
+    // http://localhost:8484/v2/api-docs
+    // http://localhost:8484/swagger-ui/index.html
+
+    private static final Contact DEFAULT_CONTACT = new Contact("Byeongju Park", "https://www.naver.com", "bottlepark28@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Web-game Backend API",
+     "BangSoft Project : Investing Web-game", 
+     "1.0", 
+     "urn:tos", DEFAULT_CONTACT, 
+     "Apache 2.0", 
+     "http://www.apache.org/licenses/LICENSE-2.0", 
+     new ArrayList<>()
+     );
+     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(DEFAULT_API_INFO)
+        .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+        .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+
+    // actuaos와 swagger의 충돌로 인한 처리
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+}
