311fafc87a2b956d7e046b0f23eac48ad2b73ebc
byeongjuPark <bottlepark28@gmail.com>
2023-07-14 17:22:17 +0900

    first commit

---
+++ src/test/java/com/bangsoft/webgame/WebGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangsoft.webgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
